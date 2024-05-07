408ca42adad91c14cddfa86177eaea4dab5cf05f
parsTroy <troyparsons@hotmail.com>
2024-05-06 21:54:35 -0400

    Buy page initiate

---
+++ src/app/buy/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <div className="space-y-4 text-center md:mt-48">BUY</div>
+    </main>
+  );
+};
+
+export default page;
