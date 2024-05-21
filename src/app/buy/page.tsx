5b62510df5711f643f567ae927a907159a7b9b5f
parsTroy <troyparsons@hotmail.com>
2024-05-21 10:03:49 -0400

    Update base

--- src/app/buy/page.tsx
+++ src/app/buy/page.tsx
@@ -2,7 +2,6 @@ const page = () => {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <div className="space-y-4 text-center md:mt-48">BUY</div>
-      Buy Now Close
     </main>
   );
 };
