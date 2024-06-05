4c5732c20a5cff618d981ee65bcc5dace1fb7fe2
parsTroy <troyparsons@hotmail.com>
2024-06-05 17:10:33 -0400

    Update base

--- src/app/buy/page.tsx
+++ src/app/buy/page.tsx
@@ -1,7 +1,7 @@
 const page = () => {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <div className="space-y-4 text-center md:mt-48">BUY</div>
+      <div className="space-y-4 text-center md:mt-48">BUY PAGE</div>
     </main>
   );
 };
